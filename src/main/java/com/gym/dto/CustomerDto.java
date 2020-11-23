4e1655c131321449afa21cc9758679da65ef54f0
Milkias <tadmilk5@gmail.com>
2020-11-23 19:22:37 +0100

    Commiting changes - add few validations in CustomerDto

--- src/main/java/com/gym/dto/CustomerDto.java
+++ src/main/java/com/gym/dto/CustomerDto.java
@@ -3,18 +3,24 @@ package com.gym.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gym.entity.Customer;
+import org.hibernate.validator.constraints.Length;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CustomerDto {
 
     @JsonProperty("personNumber")
+    @Length(max = 13, min = 9, message = "maximum 13 minimum 9 allowed number of characters")
     private String personNumber;
     @JsonProperty("firstName")
+    @Length(max = 25, message = "maximum allowed number of characters is 25.")
     private String firstName;
     @JsonProperty("middleName")
+    @Length(max = 25, message = "maximum allowed number of characters is 25.")
     private String middleName;
     @JsonProperty("lastName")
+    @Length(max = 25, message = "maximum allowed number of characters is 25.")
     private String lastName;
+    @Length(max = 15, message = "maximum allowed number of characters is 15.")
     @JsonProperty("phonenumber")
     private String phoneNumber;
     @JsonProperty("numberOfBookingAllowedPerWeek")
